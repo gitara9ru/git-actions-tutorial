39477eb754b20775f1b03d5bffbb69c72a75c836
gitara9ru <gitara9ru0611@gmail.com>
2022-09-27 21:28:41 +0900

    rollout tes

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Itemm'}
+                        {submitting ? 'Putting...' : 'Put Itemm'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
